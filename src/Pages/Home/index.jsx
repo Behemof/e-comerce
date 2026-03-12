83ab902da39b703235a293d076b28ea9b7f98a4d
Noctis <edisonnoctis@hotmail.com>
2026-03-11 22:42:56 -0500

    Cosumo de API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,25 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
 function Home (){
-
+    const [items, setItems] = useState([]);
+    useEffect (()=> {
+        fetch("https://api.escuelajs.co/api/v1/products")
+        .then(response => response.json())
+        .then(datos => setItems(datos))
+    },[])
     return(
 
         <Layout>
             Home
-            <Card/>
+            <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+
+                {items?.map(item=> ( <Card key={item.id} data={item} />))}
+            
+
+            </div>
+            
         </Layout>
     )
 }
